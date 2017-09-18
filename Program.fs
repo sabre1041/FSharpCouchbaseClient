0d47b1e89e5d9210b10d330723e6cb6c529765f2
John Krupka <john.r.krupka@gmail.com>
2017-09-18 05:55:29 -0500

    first commit

---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.Collections.Generic
+open Newtonsoft.Json
+open Couchbase
+open Couchbase.Configuration.Client
+
+let toGenericList lst = List<_>(List.toSeq lst)
+
+let fetch (cluster: Cluster) (query: string) =
+    using (cluster.OpenBucket("travel-sample")) 
+        (fun bucket -> 
+            using (bucket.Query(query))
+                (fun result -> JsonConvert.SerializeObject(result)))
+
+[<EntryPoint>]
+let main argv =
+    let uris = [Uri("http://localhost:8091")]
+    let cluster = new Cluster(ClientConfiguration(Servers = toGenericList uris, UseSsl = false))
+
+    "SELECT * FROM `travel-sample` LIMIT 10"
+    |> fetch cluster
+    |> printfn "%s"
+    0
+    
