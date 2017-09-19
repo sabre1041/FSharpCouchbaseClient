2d35b451071fed88fd9c5b355d3e0ee60b5fa49f
John Krupka <john.r.krupka@gmail.com>
2017-09-19 09:49:48 -0500

    better failure handling

--- Program.fs
+++ Program.fs
@@ -4,13 +4,17 @@ open Newtonsoft.Json
 open Couchbase
 open Couchbase.Configuration.Client
 
+type Result<'a,'b> = Success of 'a | Failure of 'b
+
 let toGenericList lst = List<_>(List.toSeq lst)
 
 let fetch (cluster: Cluster) bucketName (query: string) =
     using (cluster.OpenBucket(bucketName)) 
         (fun bucket -> 
             using (bucket.Query(query))
-                (fun result -> JsonConvert.SerializeObject(result)))
+                (fun result -> if result.Success 
+                               then Success <| JsonConvert.SerializeObject(result) 
+                               else Failure <| sprintf "%A, %A" result.Errors result.Exception))
 
 [<EntryPoint>]
 let main argv =
@@ -19,6 +23,8 @@ let main argv =
 
     ("travel-sample", "SELECT * FROM `travel-sample` LIMIT 10")
     ||> fetch cluster
-    |> printfn "%s"
+    |> function
+       | Success msg -> printfn "SUCCESS: %s" msg
+       | Failure msg -> printfn "FAILED:\n%s" msg
     0
     
